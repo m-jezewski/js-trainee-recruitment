adaf5a9902c47311d17474aaca828c2b24e23f36
Karrius <Ecetris@gmail.com>
2023-03-15 01:41:49 +0100

    styles, + index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,16 @@
+
+import { Menu } from "@/components/Menu/Menu";
+import styles from "../styles/index.module.css";
+import { AnimalSectionList } from "@/components/animalSectionList/animalSectionList";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
-import { LogoNerdbord } from "../components/LogoNerdbord";
-
-import styles from "../styles/index.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.wrapper}>
-      <LogoNerdbord />
-      <h1 className={inter.className}>
-        Recruitment task for Javascript Trainee
-      </h1>
+    <div className={`${styles.wrapper} ${inter.className}`}>
+      <Menu />
+      <AnimalSectionList />
     </div>
   );
 }
